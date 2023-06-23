296a2219360cc1b4c910fc0893900557febf5647
federico nahuel dal degan <federiconahueldaldegan@hotmail.com>
2023-06-22 21:36:55 -0300

    Me decidi por una calculaora con alerts y prompts.

--- main.js
+++ main.js
@@ -1,4 +1,47 @@
-document.addEventListener;
 
-alert("¡Bienvenid@ al quizz de HTML Y CSS!\nTu conocimiento será puesto a prueba.");
+
+
+function bienvenida(){
+
+    let usuario = prompt("Por favor, ingrese su nombre:");
+    let salu2 = ("Bienvenido " + usuario);
+    document.write("¡Hola " + usuario + "!");
+
+    if(usuario == ""){
+        alert("Debe ingresar un nombre.");
+        }else{
+            alert(salu2);
+        }
+}
+
+let salir = "ESC";
+while (salir != "Y" || salir != "y") {
+    alert(calculadora(parseFloat(prompt("Ingrese un numero: ")),
+    (parseFloat(prompt("Ingrese un segundo numero: "))), 
+    prompt("Ingrese la operacion: ")));
+    salir = prompt("Escriba ESC para salir.");
+}
+
+function calculadora(primerNum, segundoNum, operacion) {
+
+    if(operacion == "/" && segundoNum == 0){
+        alert("No es posible dividir por 0");
+    }
+    else if (operacion == "+") {
+        return primerNum + segundoNum;
+    }  else if (operacion == "-") {
+        return primerNum - segundoNum;
+    } else if (operacion == "/") {
+        return primerNum / segundoNum;
+    } else if (operacion == "*") {
+        return primerNum * segundoNum;
+    } else {
+        return ("Debe ingresar los datos pedidos.");
+    }
+}
+
+
+if(salir == "y" || salir == "Y"){
+    alert("Nos veremos pronto " + usuario);
+}
 
